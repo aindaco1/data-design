d1bbc76ba4b6d393a57cc298bdd5f1a4bc3cd8f4
Alonso Indacochea <aindaco1@hotmail.com>
2015-01-21 13:59:06 -0700

    added php and updated ddl

---
+++ php/classes/awards.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * Awards received for an album on allmusic.com
+ *
+ * This class represents industry awards received for the release, sales, or critical acclaim of an album
+ * as represented on allmusic.com.
+ *
+ * @author Alonso Indacochea <alonso@hermesdevelopment.com>
+ **/
+class Awards {
+	/**
+	 * id for this award; this is the primary key
+	 **/
+	private $awardsId;
+	/**
+	 * id of the album that received this award; this is a foreign key
+	 **/
+	private $albumId;
+	/**
+	 * year award was received
+	 **/
+	private $awardsYear;
+	/**
+	 * title of the award
+	 **/
+	private $awardsTitle;
+
+	// CONSTRUCTOR GOES HERE
+
+	/**
+	 * accessor method for award id
+	 *
+	 * @return mixed value of award id
+	 **/
+	public function getAwardsId() {
+		return ($this->awardsId);
+	}
+
+	/**
+	 * mutator method for awards id
+	 *
+	 * @param mixed $newAwardsId new value of awards id
+	 * @throws InvalidArgumentException if $newAwardsId is not an integer
+	 * @throws RangeException if $newAwardsId is not positive
+	 **/
+	public function setAwardsId($newAwardsId) {
+		if($newAwardsId === null) {
+			$this->awardsId = null;
+			return;
+		}
+		// verify the awards id is valid
+		$newAwardsId = filter_var($newAwardsId, FILTER_VALIDATE_INT);
+		if($newAwardsId === false) {
+			throw(new InvalidArgumentException("awards id is not a valid integer"));
+		}
+		// verify the awards id is positive
+		if($newAwardsId <= 0) {
+			throw(new RangeException("awards id is not positive"));
+		}
+		// convert and store the awards id
+		$this->awardsId = intval($newAwardsId);
+	}
+	/**
+	 * accessor method for album id
+	 *
+	 * @return mixed value of album id
+	 **/
+	public function getAlbumId() {
+		return ($this->albumId);
+	}
+
+	/**
+	 * mutator method for album id
+	 *
+	 * @param mixed $newAlbumId new value of album id
+	 * @throws InvalidArgumentException if $newAlbumId is not an integer
+	 * @throws RangeException if $newAlbumId is not positive
+	 **/
+	public function setAlbumId($newAlbumId) {
+		if($newAlbumId === null) {
+			$this->albumId = null;
+			return;
+		}
+		// verify the album id is valid
+		$newAlbumId = filter_var($newAlbumId, FILTER_VALIDATE_INT);
+		if($newAlbumId === false) {
+			throw(new InvalidArgumentException("album id is not a valid integer"));
+		}
+		// verify the album id is positive
+		if($newAlbumId <= 0) {
+			throw(new RangeException("album id is not positive"));
+		}
+		// convert and store the awards id
+		$this->albumId = intval($newAlbumId);
+	}
+
+	/**
+	 * accessor method for award year
+	 *
+	 * @return mixed value of award year
+	 **/
+	public function getAwardsYear() {
+		return ($this->awardsYear);
+	}
+
+	/**
+	 * mutator method for award year
+	 *
+	 * @param mixed $newAwardsYear new value of award year
+	 * @throws InvalidArgumentException if $newAwardsYear is not an integer
+	 * @throws RangeException if $newAwardsYear is a future year
+	 * @throws RangeException if $newAwardsYear is an ancient year
+	 **/
+	public function setAwardsYear($newAwardsYear) {
+		if($newAwardsYear === null) {
+			$this->awardsYear = null;
+			return;
+		}
+		// verify the award year is valid
+		$newAwardsYear = filter_var($newAwardsYear, FILTER_VALIDATE_INT);
+		if($newAwardsYear === false) {
+			throw(new InvalidArgumentException("awards year is not a valid integer"));
+		}
+		// verify the award year is not a future year
+		if($newAwardsYear > intval(date("Y"))) {
+			throw(new RangeException("awards year is a future year"));
+		}
+		// verify the award year is not an ancient year
+		if($newAwardsYear <= 1500) {
+			throw(new RangeException("awards year is an ancient year"));
+		}
+		// convert and store the awards id
+		$this->awardsYear = intval($newAwardsYear);
+	}
+	/**
+	 * accessor method for award title
+	 *
+	 * @return mixed value of award title
+	 **/
+	public function getAwardsTitle() {
+		return ($this->awardsTitle);
+	}
+
+	/**
+	 * mutator method for award title
+	 *
+	 * @param string $newAwardsTitle new value of award title
+	 * @throws InvalidArgumentException if $newAwardsTitle is not a string or insecure
+	 * @throws RangeException if $newAwardsTitle is > 200 characters
+	 **/
+	public function setAwardsTitle($newAwardsTitle) {
+		// verify the award title is secure
+		$newAwardsTitle = trim($newAwardsTitle);
+		$newAwardsTitle = filter_var($newAwardsTitle, FILTER_SANITIZE_STRING);
+		if($newAwardsTitle === false || empty($newAwardsTitle) === true) {
+			throw(new InvalidArgumentException("award title is empty or insecure"));
+		}
+
+		// verify the award title will fit in the database
+		if(strlen($newAwardsTitle) > 200) {
+			throw(new RangeException("award title too long"));
+		}
+
+		// store the award title
+		$this->awardsTitle = $newAwardsTitle;
+	}
+}
+?>
--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,42 +1,57 @@
--- this is a comment is SQL (yes, space is needed)
-
 DROP TABLE IF EXISTS album;
-DROP TABLE IF EXISTS albumGenre;
-DROP TABLE IF EXISTS genre;
-DROP TABLE IF EXISTS albumStyles;
-DROP TABLE IF EXISTS styles;
-DROP TABLE IF EXISTS albumArtist;
-DROP TABLE IF EXISTS artist;
 DROP TABLE IF EXISTS credits;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS styles;
+DROP TABLE IF EXISTS genre;
+DROP TABLE IF EXISTS albumArtist;
+DROP TABLE IF EXISTS albumStyles;
+DROP TABLE IF EXISTS albumGenre;
 
 CREATE TABLE album (
+	albumId           INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	albumAllmusicPick BOOLEAN                     NOT NULL,
+	allmusicRating    TINYINT UNSIGNED            NOT NULL,
+	duration          VARCHAR(16)                 NOT NULL,
+	recordingDate     DATE                        NOT NULL,
+	releaseDate       DATE                        NOT NULL,
+	releaseYear       YEAR                        NOT NULL,
+	streamBuy         VARCHAR(1024),
+	INDEX(albumId),
+	PRIMARY KEY (albumId)
+);
+
+CREATE TABLE artist (
+	artistId          INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	artistDescription VARCHAR(128)                NOT NULL,
+	INDEX(artistId),
+	PRIMARY KEY (artistId)
+);
+
+CREATE TABLE credits (
 	albumId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	duration VARCHAR(16) NOT NULL,
-	albumAllmusicPick BOOLEAN NOT NULL,
-	streamBuy VARCHAR(1024),
-	allmusicRating INT UNSIGNED NOT NULL,
-	releaseDate DATE NOT NULL,
-	releaseYear VARCHAR(4) NOT NULL,
-	recordingDate DATE NOT NULL,
+	artistId INT UNSIGNED NOT NULL,
+	credits VARCHAR(128),
+	INDEX(albumId),
+	INDEX(artistId),
+	FOREIGN KEY(artistId) REFERENCES artist(artistId),
+	FOREIGN KEY(albumId) REFERENCES album(albumId),
 	PRIMARY KEY(albumId)
 );
 
 CREATE TABLE genre (
-	genreId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	genreDescription VARCHAR(128) NOT NULL,
-	PRIMARY KEY(genreId)
+	genreId          INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	genreDescription VARCHAR(128)                NOT NULL,
+	PRIMARY KEY (genreId)
 );
 
-CREATE TABLE artist (
-	artistId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	artistName VARCHAR(256) NOT NULL,
-	PRIMARY KEY(artistId)
+CREATE TABLE styles (
+	stylesId          INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	stylesDescription VARCHAR(128)                NOT NULL,
+	PRIMARY KEY (stylesId)
 );
 
-
-CREATE TABLE credits (
-	credits VARCHAR(128),
-	albumId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+CREATE TABLE albumArtist (
+	albumId INT UNSIGNED NOT NULL,
 	artistId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	INDEX(albumId),
 	INDEX(artistId),
@@ -45,13 +60,23 @@ CREATE TABLE credits (
 	PRIMARY KEY(artistId, albumId)
 );
 
-
-CREATE TABLE favorite (
-	profileId INT UNSIGNED NOT NULL,
-	tweetId INT UNSIGNED NOT NULL,
-	favoriteDate DATETIME NOT NULL,
-	INDEX(profileId),
-	INDEX(tweetId),
-	FOREIGN KEY(profileId) REFERENCES profile(profileId),
-	PRIMARY KEY(profileId, tweetId)
+CREATE TABLE albumStyles (
+	albumId INT UNSIGNED NOT NULL,
+	stylesId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	INDEX(albumId),
+	INDEX(stylesId),
+	FOREIGN KEY(stylesId) REFERENCES styles(stylesId),
+	FOREIGN KEY(albumId) REFERENCES album(albumId),
+	PRIMARY KEY(stylesId, albumId)
 );
+
+CREATE TABLE albumGenre (
+	albumId INT UNSIGNED NOT NULL,
+	genreId INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	INDEX(albumId),
+	INDEX(genreId),
+	FOREIGN KEY(genreId) REFERENCES genre(genreId),
+	FOREIGN KEY(albumId) REFERENCES album(albumId),
+	PRIMARY KEY(genreId, albumId)
+);
+
